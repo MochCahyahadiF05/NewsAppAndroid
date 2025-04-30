f91c52257017c777c43e71b66dde7fd30227698d
MochCahyahadiF05 <mochcahyahadif@gmail.com>
2025-04-30 11:52:22 +0700

    add file SplashScreenActivity & LoginActivity

---
+++ app/src/main/java/com/example/newsapp/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.example.newsapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        findViewById<Button>(R.id.btnLogin).setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+}
