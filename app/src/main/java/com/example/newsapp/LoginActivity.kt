0cea5f249a613ff596a3d068b23f68dd8a3c4175
MochCahyahadiF05 <mochcahyahadif@gmail.com>
2025-05-10 00:59:25 +0700

    update tampilan splashscreen dan login

--- app/src/main/java/com/example/newsapp/LoginActivity.kt
+++ app/src/main/java/com/example/newsapp/LoginActivity.kt
@@ -1,8 +1,16 @@
 package com.example.newsapp
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.ForegroundColorSpan
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,9 +20,30 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-        findViewById<Button>(R.id.btnLogin).setOnClickListener {
-            startActivity(Intent(this, RegisterActivity::class.java))
+//        window.statusBarColor = android.graphics.Color.TRANSPARENT
+        window.apply {
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            statusBarColor = Color.TRANSPARENT
         }
+
+
+        val registerTextView = findViewById<TextView>(R.id.registerTextView)
+        val fullText = "Belum punya akun? Daftar"
+        val spannable = SpannableString(fullText)
+
+        val blueColor = ForegroundColorSpan(Color.parseColor("#0033A0"))
+        spannable.setSpan(blueColor, 18, fullText.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        spannable.setSpan(clickableSpan, 18, fullText.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        registerTextView.text = spannable
+        registerTextView.movementMethod = LinkMovementMethod.getInstance()
     }
 }
--- app/src/main/java/com/example/newsapp/SplashScreenActivity.kt
+++ app/src/main/java/com/example/newsapp/SplashScreenActivity.kt
@@ -1,9 +1,14 @@
 package com.example.newsapp
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.postDelayed
@@ -15,10 +20,26 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+//        window.statusBarColor = android.graphics.Color.TRANSPARENT
+        window.apply {
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            statusBarColor = Color.TRANSPARENT
+        }
+
+
+        val appNameText = findViewById<TextView>(R.id.appName)
+        val logoImage = findViewById<ImageView>(R.id.logoIcon)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val anim = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+            appNameText.visibility = View.VISIBLE
+            appNameText.startAnimation(anim)
+        }, 1000)
 
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
-        }, 2000)
+        }, 4000)
     }
 }
